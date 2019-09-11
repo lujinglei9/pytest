26aadc5c0e1a74a1e71357b4247a4ebd279859ac
lujinglei9 <lujinglei9@163.com>
2019-09-11 19:04:52 +0800

    4玩 Signed-off-by: lujinglei9 <lujinglei9@163.com>

---
+++ TestCase_000001/TestCase000001.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+import pytest
+
+
+class Testcase():
+	
+    def test_div_001(self):
+        assert  0
+
+    def test_div_002(self):
+        assert  1
+
+    def test_div_003(self):
+        assert  2
